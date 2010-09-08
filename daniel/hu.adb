7f8f0a1564a46780e76c856bc2c4055a02eaf206
DubioserKerl <DubioserKerl@05d21c98-fc12-a26c-e4fe-dd7fba3cfd3e>
2010-09-08 13:22:53 +0000

    jetzt auch bei mir mit contraint error

--- daniel/hu.adb
+++ daniel/hu.adb
@@ -3,21 +3,21 @@ WITH Ada.Integer_Text_IO;
 use Ada.Integer_Text_IO;
 PROCEDURE Hu IS
    Parameter: Integer;
-   MyString: String := "A";
-   MySecond: String := "B";
-   TYPE Complex IS
-   RECORD
-      RealPart: Integer :=1;
-      ImagPart: Integer:=1;
-   END RECORD;
-      C: Complex;
-   D:Complex:=(2,9);
-   E: Complex:=(RealPart=>5, others =>6);
+   --MyString: String := "A";
+   --MySecond: String := "B";
+   --TYPE Complex IS
+   --RECORD
+   --   RealPart: Integer :=1;
+   --   ImagPart: Integer:=1;
+  -- END RECORD;
+   --   C: Complex;
+   --D:Complex:=(2,9);
+   --E: Complex:=(RealPart=>5, others =>6);
 BEGIN
   -- Put(C.RealPart);
   -- Put(C.ImagPart);
 
-  -- get(Parameter);
+   get(Parameter);
    Mydemo.Demo(Parameter);
    null;
 end hu;
