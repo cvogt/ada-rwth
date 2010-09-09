ac80ee4922c5fcf524b03dd869f0ccce2bd9b83e
DubioserKerl <DubioserKerl@05d21c98-fc12-a26c-e4fe-dd7fba3cfd3e>
2010-09-09 12:37:18 +0000

    Spaßiges Gepansche it allgemeinen Pointern

--- daniel/PointerToInt.adb
+++ daniel/PointerToInt.adb
@@ -2,15 +2,15 @@ WITH Ada.Integer_Text_IO;
 USE Ada.Integer_Text_IO;
 
 PROCEDURE PointerToInt IS
-   TYPE Int_Zeiger IS ACCESS Integer range 0..10;
-   MeinPointer:Int_Zeiger;
-   ZweiterPointer: access Integer; --wenig hilfreich
+   TYPE Int_Zeiger  IS access constant Integer;
+   myVar2: aliased constant integer :=Integer'(100);
+   MeinPointer: Int_Zeiger := new Integer'(2);
+
    BEGIN
-      MeinPointer:= NEW Integer'(4);
-      ZweiterPointer := new Integer'(2);
-      --MeinPointer.all:=0;
+
+      MeinPointer := MyVar2'access;
+
       Put(MeinPointer.All);
-      Put(ZweiterPointer.All);
-      MeinPointer.all:=ZweiterPointer.all;
+
     null;
 end PointerToInt;
