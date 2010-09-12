79a55f5ba2f1474f22f67a56841e5fca0a7a744f
jan.christopher.vogt <jan.christopher.vogt@05d21c98-fc12-a26c-e4fe-dd7fba3cfd3e>
2010-09-12 22:48:40 +0000

    created a new tagged record above parent to further clarify dependencies

--- chris/inheritance_parent.ads
+++ chris/inheritance_parent.ads
@@ -1,5 +1,5 @@
 with Ada.Text_IO; use Ada.Text_IO;
+with inheritance_parents_parent;
 package inheritance_parent is
-	type Parent is tagged null record;
-	procedure test( b:Parent );
+	type Parent is new inheritance_parents_parent.Parents_Parent with null record;
 end;
---
+++ chris/inheritance_parents_parent.adb
@@ -0,0 +1,6 @@
+with Ada.Text_IO; use Ada.Text_IO;
+package body inheritance_parents_parent is
+	procedure test( b:Parents_Parent ) is begin
+		put("Parents Parent");
+	end;
+end;
