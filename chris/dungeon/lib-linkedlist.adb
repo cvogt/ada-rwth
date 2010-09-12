97ed76fe4d9101e3b905210177cec982d57b3c8b
jan.christopher.vogt <jan.christopher.vogt@05d21c98-fc12-a26c-e4fe-dd7fba3cfd3e>
2010-09-12 17:55:36 +0000

    - changed lib-linkedlist from ADT to ADO

--- chris/dungeon/lib-linkedlist.adb
+++ chris/dungeon/lib-linkedlist.adb
@@ -1,32 +1,32 @@
 separate(lib)
 package body LinkedList is
+	type TElement;
+	type PElement is access TElement;
+	type TElement is record
+		next : PElement;
+		val : Value;
+	end record;
 	OutOfBound : exception;
-	procedure create ( l:in out List) is
-		-- head is implemented as a TElement
-		-- so there is always one empty TElement
-		-- in the list which is used as head, but
-		-- does not count as a real element
-		head : PElement := new TElement;
-	begin
-		l    := new TList'(
-			head => head,
-			last => head,
-			length => 0
-		);
-	end;
-	procedure add( l:List; v:Value ) is
+	-- head is implemented as a TElement
+	-- so there is always one empty TElement
+	-- in the list which is used as head, but
+	-- does not count as a real element
+	len : INTEGER := 0;
+	head : PElement := new TElement;
+	last : PElement := head;
+	procedure add( v:Value ) is
 		e : PElement := new TElement;
 	begin
 		e.val := v;
-		l.last.next := e;
-		l.last := e;
-		l.length := l.length+1;
+		last.next := e;
+		last := e;
+		len := length+1;
 	end;
-	function get( l:List; i:INTEGER ) return Value is
+	function get( i:INTEGER ) return Value is
 		current : PElement;
 	begin
-		current := l.head;
-		for j in 1..l.length loop
+		current := head;
+		for j in 1..len loop
 			current := current.next;
 			if i = j then
 				return current.val;
@@ -34,7 +34,7 @@ package body LinkedList is
 		end loop;
 		raise OutOfBound;
 	end;
-	function length( l:List ) return INTEGER is begin
-		return l.length;
+	function length return INTEGER is begin
+		return len;
 	end;
 end LinkedList;
--- chris/dungeon/test-lib-linkedlist.adb
+++ chris/dungeon/test-lib-linkedlist.adb
@@ -1,22 +1,19 @@
 with lib; use lib.unittest;
 procedure test is
-	package int_list is new lib.linkedlist( INTEGER );
-	use int_list;
-	l : List;
+	package list is new lib.linkedlist( INTEGER );
+	use list;
 begin
-	int_list.create(l);
-		
-	assert( length(l)=0, "initial list length" );
-	add(l, 5);
-	assert( length(l)=1, "length after adding one element" );
-	assert( get(l,1)=5, "get first element" );
-	add(l, 6);
-	add(l, 7);
-	add(l, 1);
-	assert( length(l)=4, "length after adding more elements" );
-	assert( get(l,3)=7, "get element 3" );
-	assert( get(l,1)=5, "get element 1" );
-	assert( get(l,4)=1, "get element 4" );
-	assert( get(l,2)=6, "get element 2" );
+	assert( length=0, "initial list length" );
+	add(5);
+	assert( length=1, "length after adding one element" );
+	assert( get(1)=5, "get first element" );
+	add(6);
+	add(7);
+	add(1);
+	assert( length=4, "length after adding more elements" );
+	assert( get(3)=7, "get element 3" );
+	assert( get(1)=5, "get element 1" );
+	assert( get(4)=1, "get element 4" );
+	assert( get(2)=6, "get element 2" );
 	
 end;
