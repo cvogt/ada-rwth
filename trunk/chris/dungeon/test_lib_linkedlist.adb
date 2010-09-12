49146e49adef63c97f6be2a754289b23034989af
jan.christopher.vogt <jan.christopher.vogt@05d21c98-fc12-a26c-e4fe-dd7fba3cfd3e>
2010-09-12 19:15:50 +0000

    added combined runner for unittests added make as make and run shortcut

---
+++ trunk/chris/dungeon/test_lib_linkedlist.adb
@@ -0,0 +1,19 @@
+with lib.linkedlist; with lib.unittest; use lib.unittest;
+procedure test_lib_linkedlist is
+	package list is new lib.linkedlist( INTEGER );
+	use list;
+begin
+	assert( length=0, "initial list length" );
+	add(5);
+	assert( length=1, "length after adding one element" );
+	assert( get(1)=5, "get first element" );
+	add(6);
+	add(7);
+	add(1);
+	assert( length=4, "length after adding more elements" );
+	assert( get(3)=7, "get element 3" );
+	assert( get(1)=5, "get element 1" );
+	assert( get(4)=1, "get element 4" );
+	assert( get(2)=6, "get element 2" );
+	
+end;
