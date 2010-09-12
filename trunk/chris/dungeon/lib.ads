bc9e25993b3cc7c4b2d2690420f51d60d8943705
jan.christopher.vogt <jan.christopher.vogt@05d21c98-fc12-a26c-e4fe-dd7fba3cfd3e>
2010-09-12 17:55:36 +0000

    - changed lib-linkedlist from ADT to ADO

--- trunk/chris/dungeon/lib.ads
+++ trunk/chris/dungeon/lib.ads
@@ -3,24 +3,9 @@ package lib is
 	generic
 		type Value is private;
 	package LinkedList is
-		type List is private;
-		procedure create( l:in out List );
-		procedure add( l:List; v:Value );
-		function get( l:List; i:INTEGER ) return Value;
-		function length( l:List ) return INTEGER;
-	private
-		type TElement;
-		type PElement is access TElement;
-		type TElement is record
-			next : PElement;
-			val : Value;
-		end record;
-		type TList is record
-			head : PElement;
-			last : PElement;
-			length : INTEGER := 0;
-		end record;
-		type List is access TList;
+		procedure add( v:Value );
+		function get( i:INTEGER ) return Value;
+		function length return INTEGER;
 	end;
 	package UnitTest is
 		procedure fail( s:STRING := "" );
